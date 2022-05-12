5a1cc1097178a8bc69e07008135361d575e91c6b
nakatayudai <102974156+nakatayudai@users.noreply.github.com>
2022-05-12 04:35:02 +0000

    upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'lists/neoller'
-  get 'lists/lists'
   get 'lists/new'
+  get 'top' => 'home#top'
+  
   post 'lists' => 'lists#create'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
-
+  get 'lists' => 'lists#index'
+  get 'lists/:id/edit' => 'lists#edit', as:'edit_list'
+  get 'lists/:id' => 'lists#show' , as: 'list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end

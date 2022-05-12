3690268189ee5180137698de44c1b0313bf3c413
nakatayudai <102974156+nakatayudai@users.noreply.github.com>
2022-05-12 16:36:45 +0000

    upload

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -28,9 +28,18 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to 'lists'
   end
 
   private
